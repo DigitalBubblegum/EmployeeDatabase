4dbd9e844ef8a9dabfc5821148cb9a1875ada526
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-05-28 11:51:43 +0530

    -fixed the bug and added api for get and post req

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const config = require('./utils/config')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
 const eRecordsRouter = require('./controllers/eRecords')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+
+morgan.token('mes', function getMes(request) {
+	return JSON.stringify(request.body)
+})
+
 mongoose.set('strictQuery',false)
 const url = process.env.MONGODB_URI
 logger.info('connecting to', config.MONGODB_URI)
@@ -18,9 +24,10 @@ mongoose
 		logger.error('error connecting to MongoDB:', error.message)
 	})
 
-app.use(cors)
+app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :mes'))
 app.use(middleware.requestLogger)
 app.use('/api/records', eRecordsRouter)
 app.use(middleware.unknownEndpoint)
--- controllers/eRecords.js
+++ controllers/eRecords.js
@@ -1,46 +1,43 @@
 const eRecordsRouter = require('express').Router()
-const ERecord = require('../models/eRecord')
+const Employee = require('../models/eRecord')
 const logger = require('../utils/logger')
 //fetch employee records from mongoDB
-logger.info('running express')
 eRecordsRouter.get('/', (request, response, next) => {
-	logger.info(request.body)
-	ERecord.find({})
+	Employee.find({})
 		.then((record) => {
-			logger.info(record)
 			response.json(record)
 		})
 		.catch((error) => {
 			next(error)
 		})
 })
-//save employee records to mongoDB
-// eRecordsRouter.post('/',(request,response,next) => {
-// 	const body = request.body
-// 	if (body === undefined) {
-// 		return response.status(400).json({
-// 			error:'one of the field you have entered was undefined'
-// 		})
-// 	}
-// 	const eRecord = new ERecord({
-// 		name: body.name,
-// 		i_number: body.i_number,
-// 		dep_number: body.dep_number,
-// 		desig: body.desig,
-// 		phone_num: body.phone_num,
-// 	})
-// 	logger.info(body.name)
-// 	logger.info(body.i_number)
-// 	logger.info(body.dep_number)
-// 	logger.info(body.desig)
-// 	logger.info(body.phone_num)
-// 	eRecord.save()
-// 		.then(savedrecord => {
-// 			response.json(savedrecord)
-// 			logger.info('saved not to db')
-// 		})
-// 		.catch(error => next(error))
-// })
+// save employee records to mongoDB
+eRecordsRouter.post('/',(request,response,next) => {
+	const body = request.body
+	if (body === undefined) {
+		return response.status(400).json({
+			error:'one of the field you have entered was undefined'
+		})
+	}
+	const eRecord = new Employee({
+		name: body.name,
+		i_number: body.i_number,
+		dep_number: body.dep_number,
+		desig: body.desig,
+		phone_num: body.phone_num,
+	})
+	logger.info(body.name)
+	logger.info(body.i_number)
+	logger.info(body.dep_number)
+	logger.info(body.desig)
+	logger.info(body.phone_num)
+	eRecord.save()
+		.then(savedrecord => {
+			response.json(savedrecord)
+			logger.info('saved not to db')
+		})
+		.catch(error => next(error))
+})
 
 
 module.exports = eRecordsRouter
